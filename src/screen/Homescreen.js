1c9165a83472ebabd0a6059bde1263b2146bc1c5
OKUNTIMI <okuntimi@gmail.com>
2024-10-25 16:38:18 +0100

    still have error but on it

---
+++ src/screen/Homescreen.js
@@ -0,0 +1,13 @@
+import './Homescreen.css';
+import image from '.coglogo.jpg';
+function Homescreen(){
+    return(
+        <div className="homescreen">
+            <div classNmae="top">
+                <img>
+                </img>
+            </div>
+        </div>
+    );
+}
+export default Homescreen;
